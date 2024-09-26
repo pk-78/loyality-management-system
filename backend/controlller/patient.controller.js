bc370a9127687219023e2b42a47153e5ed407a49
Satyam Singh <116868686+satyamsingh22@users.noreply.github.com>
2024-09-26 13:35:46 +0530

    get all patient

--- backend/controlller/patient.controller.js
+++ backend/controlller/patient.controller.js
@@ -32,3 +32,19 @@ export const registerPatient = async (req, res) => {
     }
   };
   
+
+
+  import Patient from "../models/patient.model.js";
+
+// Register a new patient
+
+
+// Get all patients
+export const getAllPatients = async (req, res) => {
+    try {
+        const patients = await Patient.find();
+        res.status(200).json({ patients });
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error" });
+    }
+};
