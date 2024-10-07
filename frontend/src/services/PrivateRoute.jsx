3cd0e33e5be9fdd2183485e391bee29aa0be256b
Priyanshu Kushwaha <priyanshukushwaha311@gmail.com>
2024-10-07 17:36:22 +0530

    private Route set

--- frontend/src/services/PrivateRoute.jsx
+++ frontend/src/services/PrivateRoute.jsx
@@ -1,40 +1,22 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Navigate } from "react-router-dom";
-import axios from "axios";
-import { url } from "./Url";
 
-const PrivateRoutes = ({ children }) => {
-  const [isAuthenticate, setIsAuthenticate] = useState(false);
-  const [loading, setLoading] = useState(true);
+const LoginPrivateRoute = ({ children }) => {
+  const userData = JSON.parse(localStorage.getItem("userData")) || null;
 
-  useEffect(() => {
-    const checkAuth = async () => {
-      try {
-        const token = localStorage.getItem("token");
-        if (!token) {
-          setIsAuthenticate(false);
-        } else {
-          const res = await axios.get(`${url}/api/v1/user/verifytoken`, {
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          });
-          setIsAuthenticate(res.data.success);
-        }
-      } catch (error) {
-        setIsAuthenticate(false);
-      } finally {
-        setLoading(false);
-      }
-    };
-    checkAuth();
-  }, []);
-
-  if (loading) {
-    return <div>Please Wait .....</div>; // Consider a better loading UI
+  if (!userData) {
+    return children;
   }
 
-  return isAuthenticate ? children : <Navigate to="/login" />;
+ 
+
+  if (userData && userData.role === "Staff") {
+    return <Navigate to="/patient-search" />;
+  }
+
+
+
+  return children;
 };
 
-export default PrivateRoutes;
+export default LoginPrivateRoute;
